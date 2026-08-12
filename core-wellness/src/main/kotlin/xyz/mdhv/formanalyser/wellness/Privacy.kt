13752a4e957075d58519cd1302e60e9f0c3bf44a
Madhav Baliga <42736071+mbaliga@users.noreply.github.com>
2026-08-13 04:09:42 +0530

    preserve Crocodyl private-table invariant

--- core-wellness/src/main/kotlin/xyz/mdhv/formanalyser/wellness/Privacy.kt
+++ core-wellness/src/main/kotlin/xyz/mdhv/formanalyser/wellness/Privacy.kt
@@ -6,7 +6,7 @@ object PrivacyRegistry {
         put("athlete", PrivacyClass.SHAREABLE); put("session", PrivacyClass.SHAREABLE); put("shot", PrivacyClass.SHAREABLE); put("rig", PrivacyClass.SHAREABLE)
         put("score_session", PrivacyClass.SHAREABLE); put("score_arrow", PrivacyClass.SHAREABLE); put("score_opponent_end", PrivacyClass.SHAREABLE)
         put("score_candidate", PrivacyClass.SHAREABLE); put("observer_score_event", PrivacyClass.SHAREABLE)
-        put("goal", PrivacyClass.SHAREABLE); put("intervention", PrivacyClass.SHAREABLE); put("training_plan", PrivacyClass.SHAREABLE); put("session_context", PrivacyClass.SHAREABLE); put("session_default", PrivacyClass.PRIVATE)
+        put("goal", PrivacyClass.SHAREABLE); put("intervention", PrivacyClass.SHAREABLE); put("training_plan", PrivacyClass.SHAREABLE); put("session_context", PrivacyClass.SHAREABLE); put("session_default", PrivacyClass.SHAREABLE)
         put("checkin", PrivacyClass.SHAREABLE); put("soreness", PrivacyClass.SHAREABLE); put("rest_day", PrivacyClass.SHAREABLE); put("hiatus", PrivacyClass.SHAREABLE); put("event", PrivacyClass.SHAREABLE)
         put("mood_entry", PrivacyClass.PRIVATE); put("life_event", PrivacyClass.PRIVATE); put("cycle_entry", PrivacyClass.PRIVATE); put("medication_entry", PrivacyClass.MEDICAL)
         put("pain_log", PrivacyClass.SHAREABLE); put("injury", PrivacyClass.SHAREABLE); put("physio_plan", PrivacyClass.SHAREABLE); put("physio_exercise", PrivacyClass.SHAREABLE); put("physio_session", PrivacyClass.SHAREABLE); put("document", PrivacyClass.MEDICAL)
